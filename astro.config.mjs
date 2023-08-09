910cadefc7056f9036efdf3312742a8034b3fdb9
rant <rant@rants-MacBook-Air.local>
2023-08-09 10:41:10 +1000

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify/functions";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
   adapter: netlify()
 });
